7e5d465be1bf5501951cffd6d332351441909d26
David Kim <kim.david.j@gmail.com>
2020-05-14 16:10:58 -0700

    Changed dist output path

--- server.js
+++ server.js
@@ -7,31 +7,31 @@ const path = require('path');
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-    return function(req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect(
-                ['https://', req.get('Host'), req.url].join('')
-            );
-        }
-        next();
-    }
-}
+// // If an incoming request uses
+// // a protocol other than HTTPS,
+// // redirect that request to the
+// // same url but with HTTPS
+// const forceSSL = function() {
+//     return function(req, res, next) {
+//         if (req.headers['x-forwarded-proto'] !== 'https') {
+//             return res.redirect(
+//                 ['https://', req.get('Host'), req.url].join('')
+//             );
+//         }
+//         next();
+//     }
+// }
 
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+// // Instruct the app
+// // to use the forceSSL
+// // middleware
+// app.use(forceSSL());
 
 // Start the app by listening on the default
 // Heroku port
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
 // For all GET requests, send back index.html

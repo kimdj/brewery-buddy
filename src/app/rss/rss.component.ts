1ada3ff1ce13617c45f0f269834304d86791248e
Joe Devlin <devlin3@pdx.edu>
2020-05-23 14:43:18 -0700

    Added placeholder for RSS feed

---
+++ src/app/rss/rss.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-rss',
+  templateUrl: './rss.component.html',
+  styleUrls: ['./rss.component.scss']
+})
+export class RssComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      return [
+        { title: 'Card 1'},
+        { title: 'Card 2'},
+        { title: 'Card 3'},
+        { title: 'Card 4'}
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}

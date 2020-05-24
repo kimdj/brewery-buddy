b058194303110578f0db8e7fd8055c4e17c21620
Joe Devlin <devlin3@pdx.edu>
2020-05-24 16:44:57 -0700

    Added a placeholder for the side bar RSS feed

--- src/app/rss/rss.component.ts
+++ src/app/rss/rss.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import * as Parser from "rss-parser";
+import {Observable} from "rxjs";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import { HttpClient } from "@angular/common/http";
+
+interface Beer {
+  title:string;
+  link:string;
+  published:string;
+}
 
 @Component({
   selector: 'app-rss',
@@ -7,25 +16,26 @@ import * as Parser from "rss-parser";
   styleUrls: ['./rss.component.scss']
 })
 export class RssComponent implements OnInit{
-  cards =  [
-    { title: 'Card 1'},
-    { title: 'Card 2'},
-    { title: 'Card 3'},
-    { title: 'Card 4'}
-  ];
+  beers: Array<Beer>
+  
 
-  constructor() {
+  constructor(private http:HttpClient) {
+    this.beers = new Array();
   }
   ngOnInit(): void {
     console.log("here");
-    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
-    let parser = new Parser();
-    parser.parseURL(CORS_PROXY + 'https://www.reddit.com/.rss', function(err, feed) {
-    if (err) throw err;
-      console.log(feed.title);
-      feed.items.forEach(function(entry) {
-        console.log(entry.title + ':' + entry.link);
-      })
+    //
+    this.http.get<any>('https://rss2json.com/api.json?rss_url=http://thefullpint.libsyn.com/rss').subscribe(res =>{
+      for(var index in res["items"]){
+        const episode = res["items"][index];
+        console.log(episode);
+        const beer: Beer ={
+          title: episode["title"],
+          link: episode["link"],
+          published: episode["{{beer.description}}"]
+        };
+        this.beers.push(beer);
+      }
     })
   }
 }

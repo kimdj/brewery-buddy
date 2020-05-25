588a38443b0d96e18f6f33ce11d0ba0b7093d9c5
David Kim <kim.david.j@gmail.com>
2020-05-25 11:10:30 -0700

    Re-designed navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { SpinnerService } from './services/spinner.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,46 @@ import { SpinnerService } from './services/spinner.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'brewery-buddy';
 
-  constructor(private spinner: SpinnerService) {
+  title = 'Brewery Buddy';
+  innerWidth = 0;
+  isMobileHeaderHidden = '0';
+  previousPageYOffset = 0;
+  currentPageYOffset = window.pageYOffset;
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth);
+  }
+
+  @HostListener('window:scroll', ['$event']) 
+  doSomething(event) {
+    this.currentPageYOffset = window.pageYOffset;
+
+    console.log("Scroll Event", window.pageYOffset);
+    console.log("Scroll Event", this.previousPageYOffset);
+
+    // If scrolling up or at the top, reveal the navbar.  Otherwise, hide it.
+    if ((this.previousPageYOffset > window.pageYOffset) || (window.pageYOffset <= 0)) {
+      this.isMobileHeaderHidden = '0';
+      // console.log('SCROLLING UP');
+    } else {
+      this.isMobileHeaderHidden = '-500px';
+      // console.log('SCROLLING DOWN');
+    }
+
+    this.previousPageYOffset = window.pageYOffset;
+  }
+
+  constructor(private spinner: SpinnerService,
+              private sanitizer: DomSanitizer) {
 
   }
 
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
     this.spinner.on();
   }
+
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  title = 'app';
+  innerWidth = 0;
+  isMobileHeaderHidden = '0';
+  previousPageYOffset = 0;
+  currentPageYOffset = window.pageYOffset;
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth);
   }
 
+  @HostListener('window:scroll', ['$event']) 
+  doSomething(event) {
+    this.currentPageYOffset = window.pageYOffset;
+
+    console.log("Scroll Event", window.pageYOffset);
+    console.log("Scroll Event", this.previousPageYOffset);
+
+    // If scrolling up or at the top, reveal the navbar.  Otherwise, hide it.
+    if ((this.previousPageYOffset > window.pageYOffset) || (window.pageYOffset <= 0)) {
+      this.isMobileHeaderHidden = '0';
+      // console.log('SCROLLING UP');
+    } else {
+      this.isMobileHeaderHidden = '-500px';
+      // console.log('SCROLLING DOWN');
+    }
+
+    this.previousPageYOffset = window.pageYOffset;
+  }
+
+  constructor(private sanitization: DomSanitizer) { }
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+  }
 }
